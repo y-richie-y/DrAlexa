1f8a414c413bea38dc8338fbc65df2973f7e629c
Alexander Wettig <wettig.alexander@gmail.com>
2018-01-20 23:37:14 +0000

    stupid fix

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-var alexa = require('alexa');
+var alexa = require('./alexa.js');
 app.route('/alexa').post(alexa)
 
 app.listen(port);
