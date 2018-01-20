3738e9691f3a46e312560743792f85b3ae46b206
Alexander Wettig <wettig.alexander@gmail.com>
2018-01-20 23:43:39 +0000

    proper post

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,6 @@ bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
-});
-
 var alexa = require('./alexa.js');
 app.post('/alexa', alexa)
 
