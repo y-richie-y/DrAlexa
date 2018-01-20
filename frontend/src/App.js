b440065c12c747d2ffd6bdfa7f76f8e34640279b
Richie Yeung <ycry2@cam.ac.uk>
2018-01-20 18:21:42 +0000

    Added react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from 'react-bootstrap/lib/Button';
+import Jumbotron from 'react-bootstrap/lib/Jumbotron';
+
 class App extends Component {
-  render() {
+  render(jumbotronInstance) {
     return (
       <div className="App">
         <header className="App-header">
@@ -12,10 +15,36 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br />
+          <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button bsStyle="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
         </p>
       </div>
     );
   }
 }
+
+const jumbotronInstance = (
+	<Jumbotron>
+		<h1>Hello, world!</h1>
+		<p>
+			This is a simple hero unit, a simple jumbotron-style component for calling
+			extra attention to featured content or information.
+		</p>
+		<p>
+			<Button bsStyle="primary">Learn more</Button>
+		</p>
+	</Jumbotron>
+);
+
+// render(jumbotronInstance);
 
 export default App;
