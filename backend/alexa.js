9bb80a7fbe46b4027c51587a672e9ad2a9b9ec99
Alexander Wettig <wettig.alexander@gmail.com>
2018-01-20 23:25:54 +0000

    alexa routes

---
+++ backend/alexa.js
@@ -0,0 +1,25 @@
+module.exports = function() {
+    app.route('/alexa')
+      .post((req, res) => {
+        let patient = req.body.patient;
+        let organisation = req.body.organisation;
+        let category = req.body.category;
+        let meta = req.body.meta;
+    
+        switch (category) {
+            case "prescription":
+            break;
+            case "allergy":
+            break;
+            case "condition":
+            break;
+            case "insurance":
+            break;
+            case "familyhistory":
+            break;
+        }
+    
+        res.send(req.body);
+    
+    });
+}
