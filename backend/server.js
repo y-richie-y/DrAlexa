32cb4fffdbd576c4bcdb4ee2846bb2526a8d6eb5
Alexander Wettig <wettig.alexander@gmail.com>
2018-01-20 23:27:31 +0000

    refactoring

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,9 @@ app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
+var alexa = require('alexa');
+app.route('/alexa').post(alexa)
+
 app.listen(port);
 console.log('RESTful API server started on: ' + port);
 
